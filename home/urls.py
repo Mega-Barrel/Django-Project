25863fbeb4eb6617c182a728dc4c6e18f85f5764
Mega-Barrel <saurabhjoshi24business@gmail.com>
2020-12-08 15:43:40 +0530

    Modified and Removed Code

--- home/urls.py
+++ home/urls.py
@@ -3,9 +3,21 @@ from django.urls import path
 from home import views
 
 urlpatterns = [
+    # Home Pages
+
     path('',views.index,name='home'),
-    path('about',views.about,name='about'),
-    path('package',views.package,name='package'),
-    path('contact',views.contact,name='contact'),
+    path('about', views.about, name='about'),
+    path('package', views.package, name='package'),
+    path('contact', views.contact, name='contact'),
+    path('register_signin', views.register_signin, name='register_signin'),
+
+    # Places
+    
+    path('paris', views.paris, name='paris'),
+    path('cyprus', views.cyprus, name='cyprus'),
+    path('los_angeles', views.los_angeles, name='los_angeles'),
+    path('venice', views.venice, name='venice'),
+    path('prague', views.prague, name='prague'),
+    path('tokyo', views.tokyo, name='tokyo'),
     
 ]
--- home/views.py
+++ home/views.py
@@ -3,10 +3,33 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
 def package(request):
     return render(request, 'package.html')
+
 def about(request):
     return render(request, 'about.html')
+
 def contact(request):
     return render(request, 'contact.html')
 
+def paris(request):
+    return render(request, 'paris.html')
+
+def cyprus(request):
+    return render(request, 'cyprus.html')
+
+def venice(request):
+    return render(request, 'venice.html')
+
+def tokyo(request):
+    return render(request, 'tokyo.html')
+
+def los_angeles(request):
+    return render(request, 'los_angeles.html')
+
+def prague(request):
+    return render(request, 'prague.html')
+
+def register_signin(request):
+    return render(request, 'Register.html')
